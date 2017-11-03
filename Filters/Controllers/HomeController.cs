87d17f16e3b692f80688103e8893a97955a79e0c
Anthony Leif <tonyleif@hotmail.com>
2017-11-02 22:02:58 -0500

    Page 467

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Mvc;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
     public class HomeController : Controller
     {
+        [CustomAuth(true)]
         public string Index()
         {
             return "This is the Index action on the Home controller";
