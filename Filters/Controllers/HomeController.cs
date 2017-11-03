b640f2000c9005a88312081b68de1fcfe10e7f3a
Anthony Leif <tonyleif@hotmail.com>
2017-11-02 21:30:14 -0500

    Page 465

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+    }
+}
