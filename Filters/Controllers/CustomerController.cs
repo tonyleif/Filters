06be04356d2e498df6b6ad590f65bbce51948a67
Anthony Leif <tonyleif@hotmail.com>
2017-11-05 20:28:29 -0600

    Page 499

--- Filters/Controllers/CustomerController.cs
+++ Filters/Controllers/CustomerController.cs
@@ -3,13 +3,20 @@ using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
+    [SimpleMessage(Message = "A")]
     public class CustomerController : Controller
     {
-        [SimpleMessage(Message = "A", Order = 1)]
-        [SimpleMessage(Message = "B", Order = 2)]
+        //[SimpleMessage(Message = "A", Order = 1)]
+        //[SimpleMessage(Message = "B", Order = 2)]
         public string Index()
         {
             return "This is the Customer controller";
+        }
+
+        [SimpleMessage(Message = "B")]
+        public string OtherAction()
+        {
+            return "This is the other Action in the Customer controller";
         }
     }
 }
--- Filters/Infrastructure/SimpleMessageAttribute.cs
+++ Filters/Infrastructure/SimpleMessageAttribute.cs
@@ -3,7 +3,8 @@ using System.Web.Mvc;
 
 namespace Filters.Infrastructure
 {
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    //[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public class SimpleMessageAttribute : FilterAttribute, IActionFilter
     {
         public string Message { get; set; }
